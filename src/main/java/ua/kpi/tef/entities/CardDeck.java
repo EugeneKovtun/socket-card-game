12b039d13f668d5526af1f4644fd13cdcd1e1e41
Unknown <eugene.kraros@gmail.com>
2018-04-30 15:30:43 +0300

    created method for shuffling cards

--- src/main/java/ua/kpi/tef/entities/CardDeck.java
+++ src/main/java/ua/kpi/tef/entities/CardDeck.java
@@ -1,5 +1,7 @@
 package ua.kpi.tef.entities;
 
+import java.util.Random;
+
 import static ua.kpi.tef.entities.CardRating.*;
 import static ua.kpi.tef.entities.CardSuit.*;
 
@@ -13,6 +15,8 @@ public class CardDeck {
             Card.builder().rating(JACK).suit(SPADES).build(),
             Card.builder().rating(QUINE).suit(SPADES).build(),
             Card.builder().rating(KING).suit(SPADES).build(),
+            Card.builder().rating(ACE).suit(SPADES).build(),
+
 
             Card.builder().rating(SIX).suit(CLUBS).build(),
             Card.builder().rating(SEVEN).suit(CLUBS).build(),
@@ -22,6 +26,8 @@ public class CardDeck {
             Card.builder().rating(JACK).suit(CLUBS).build(),
             Card.builder().rating(QUINE).suit(CLUBS).build(),
             Card.builder().rating(KING).suit(CLUBS).build(),
+            Card.builder().rating(ACE).suit(CLUBS).build(),
+
 
             Card.builder().rating(SIX).suit(DIAMONDS).build(),
             Card.builder().rating(SEVEN).suit(DIAMONDS).build(),
@@ -31,15 +37,27 @@ public class CardDeck {
             Card.builder().rating(JACK).suit(DIAMONDS).build(),
             Card.builder().rating(QUINE).suit(DIAMONDS).build(),
             Card.builder().rating(KING).suit(DIAMONDS).build(),
+            Card.builder().rating(ACE).suit(DIAMONDS).build(),
 
-            Card.builder().rating(SIX).suit(DIAMONDS).build(),
-            Card.builder().rating(SEVEN).suit(DIAMONDS).build(),
-            Card.builder().rating(EIGHT).suit(DIAMONDS).build(),
-            Card.builder().rating(NINE).suit(DIAMONDS).build(),
-            Card.builder().rating(TEN).suit(DIAMONDS).build(),
-            Card.builder().rating(JACK).suit(DIAMONDS).build(),
-            Card.builder().rating(QUINE).suit(DIAMONDS).build(),
-            Card.builder().rating(KING).suit(DIAMONDS).build(),
 
+            Card.builder().rating(SIX).suit(HEARTS).build(),
+            Card.builder().rating(SEVEN).suit(HEARTS).build(),
+            Card.builder().rating(EIGHT).suit(HEARTS).build(),
+            Card.builder().rating(NINE).suit(HEARTS).build(),
+            Card.builder().rating(TEN).suit(HEARTS).build(),
+            Card.builder().rating(JACK).suit(HEARTS).build(),
+            Card.builder().rating(QUINE).suit(HEARTS).build(),
+            Card.builder().rating(KING).suit(HEARTS).build(),
+            Card.builder().rating(ACE).suit(HEARTS).build(),
     };
+
+    public void shuffle() {
+        Random random = new Random();
+        for (int i = 0; i < cards.length; i++) {
+            int randomIndex = random.nextInt(cards.length);
+            Card temp = cards[i];
+            cards[i] = cards[randomIndex];
+            cards[randomIndex] = temp;
+        }
+    }
 }
