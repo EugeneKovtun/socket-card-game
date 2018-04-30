03a47130ca90ded8f5fb29a98c669a51c883c2a8
Unknown <eugene.kraros@gmail.com>
2018-04-30 17:48:46 +0300

    Created Main connected junit

--- src/main/java/ua/kpi/tef/Main.java
+++ src/main/java/ua/kpi/tef/Main.java
@@ -1,6 +1,26 @@
 package ua.kpi.tef;
 
+import ua.kpi.tef.entities.Card;
+import ua.kpi.tef.entities.CardDeck;
+import ua.kpi.tef.entities.CardRating;
+import ua.kpi.tef.entities.CardSuit;
+import ua.kpi.tef.utils.CardUtils;
+
 public class Main {
     public static void main(String[] args) {
+        CardDeck deck = new CardDeck();
+        deck.setTrump();
+        System.out.println(deck.getTrumpCard());
+        for (int i = 0; i <36 ; i++) {
+            System.out.println(deck.getAmountOfCardsInDeck());
+            System.out.println(deck.pull());
+        }
+        System.out.println(deck.getAmountOfCardsInDeck());
+        Card card1 = Card.builder().rating(CardRating.SIX).suit(CardSuit.SPADES).build();
+        Card card2 = Card.builder().rating(CardRating.ACE).suit(CardSuit.SPADES).build();
+        System.out.println(CardUtils.compareCardWithTrump(card1, card2, CardSuit.CLUBS));
+
     }
+
+
 }
